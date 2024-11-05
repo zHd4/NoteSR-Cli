a6962c76f6019637ebd8aa997b1aefcba39bce5e
zHd4 <computersystembase@gmail.com>
2024-11-05 14:27:28 +0100

    Create beforeAll in BackupDecryptorTest

--- src/test/java/app/notesr/cli/crypto/BackupDecryptorTest.java
+++ src/test/java/app/notesr/cli/crypto/BackupDecryptorTest.java
@@ -1,5 +1,9 @@
 package app.notesr.cli.crypto;
 
+import org.junit.jupiter.api.BeforeAll;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,6 +11,29 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 class BackupDecryptorTest {
+    private static SecretKey key;
+    private static byte[] salt;
+
+    private static FileInputStream encryptedBackupInputStream;
+    private static FileOutputStream tempDecryptedBackupOutputStream;
+
+    private static Path tempDecryptedBackupFilePath;
+
+    @BeforeAll
+    public static void beforeAll() throws IOException {
+        byte[] keyBytes = readFixture("aes256-key");
+
+        key = new SecretKeySpec(keyBytes, 0, keyBytes.length, Aes.KEY_GENERATOR_ALGORITHM);
+        salt = readFixture("aes256-salt");
+
+        encryptedBackupInputStream = getFixtureFileInputStream("encrypted.notesr.bak");
+
+        String tempPath = System.getProperty("java.io.tmpdir");
+
+        tempDecryptedBackupFilePath = Path.of(tempPath, "test-decrypted.notesr.bak");
+        tempDecryptedBackupOutputStream = getOutputInputStream(tempDecryptedBackupFilePath);
+    }
+
     private static FileInputStream getFixtureFileInputStream(String filename) throws IOException {
         return (FileInputStream) Files.newInputStream(generateFixturePath(filename));
     }
