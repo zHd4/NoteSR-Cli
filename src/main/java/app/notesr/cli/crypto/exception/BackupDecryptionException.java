ecfc725d5d8fb784b6ba9507c4c9a98b7a5f4514
zHd4 <computersystembase@gmail.com>
2024-11-05 11:06:42 +0100

    Create BackupDecryptionException

---
+++ src/main/java/app/notesr/cli/crypto/exception/BackupDecryptionException.java
@@ -0,0 +1,7 @@
+package app.notesr.cli.crypto.exception;
+
+public class BackupDecryptionException extends Exception {
+    public BackupDecryptionException(Exception e) {
+        super(e);
+    }
+}
