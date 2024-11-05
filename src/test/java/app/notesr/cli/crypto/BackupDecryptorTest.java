6854e78087fe7af68e001ca7f15042dc7daba303
zHd4 <computersystembase@gmail.com>
2024-11-05 14:08:41 +0100

    Create getOutputInputStream in BackupDecryptorTest

--- src/test/java/app/notesr/cli/crypto/BackupDecryptorTest.java
+++ src/test/java/app/notesr/cli/crypto/BackupDecryptorTest.java
@@ -1,6 +1,7 @@
 package app.notesr.cli.crypto;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,6 +9,10 @@ import java.nio.file.Path;
 class BackupDecryptorTest {
     private static FileInputStream getFixtureFileInputStream(String filename) throws IOException {
         return (FileInputStream) Files.newInputStream(generateFixturePath(filename));
+    }
+
+    private static FileOutputStream getOutputInputStream(Path path) throws IOException {
+        return (FileOutputStream) Files.newOutputStream(path);
     }
 
     private static byte[] readFixture(String filename) throws IOException {
