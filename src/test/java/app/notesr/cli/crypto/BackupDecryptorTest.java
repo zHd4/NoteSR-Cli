f772154d1f2d260ae85a95d06e726ae626598f68
zHd4 <computersystembase@gmail.com>
2024-11-05 15:51:02 +0100

    Implement sha256OfFile in BackupDecryptorTest

--- src/test/java/app/notesr/cli/crypto/BackupDecryptorTest.java
+++ src/test/java/app/notesr/cli/crypto/BackupDecryptorTest.java
@@ -11,8 +11,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 class BackupDecryptorTest {
+    private static final String DECRYPTED_BACKUP_HASH =
+            "b7c8a729d50b341abdedcc731a409b5dd46456b2719889ff9cf004abbb8054cf";
+
     private static SecretKey key;
     private static byte[] salt;
 
@@ -46,5 +51,26 @@ class BackupDecryptorTest {
 
     private static Path generateFixturePath(String filename) {
         return Path.of("src/test/resources/fixtures/" + filename);
+    }
+
+    private static String sha256OfFile(String path) throws NoSuchAlgorithmException, IOException {
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+
+        try (FileInputStream stream = new FileInputStream(path)) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = stream.read(buffer)) != -1) {
+                sha256.update(buffer, 0, bytesRead);
+            }
+        }
+
+        StringBuilder hexString = new StringBuilder();
+
+        for (byte b : sha256.digest()) {
+            hexString.append(String.format("%02x", b));
+        }
+
+        return hexString.toString();
     }
 }
