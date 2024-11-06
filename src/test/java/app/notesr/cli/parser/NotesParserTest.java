512c112eb0274cbdcc6f639c22c39adf1f0c478d
zHd4 <computersystembase@gmail.com>
2024-11-06 16:54:33 +0100

    Create NotesParserTest

---
+++ src/test/java/app/notesr/cli/parser/NotesParserTest.java
@@ -0,0 +1,15 @@
+package app.notesr.cli.parser;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class NotesParserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
