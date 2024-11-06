a1e06c48cd3f56e946c3d366bbed03960ff87b17
zHd4 <computersystembase@gmail.com>
2024-11-06 16:52:37 +0100

    Add EqualsAndHashCode annotation to NoteInfo

--- src/main/java/app/notesr/cli/model/NoteInfo.java
+++ src/main/java/app/notesr/cli/model/NoteInfo.java
@@ -1,10 +1,12 @@
 package app.notesr.cli.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class NoteInfo {
     private long id;
     private String title;
