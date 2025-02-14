6da4dfdb5cb830a8544b9343cd1e22794f09bd0d
zHd4 <computersystembase@gmail.com>
2025-02-14 18:56:46 +0100

    Refactor NotesParserTest

--- src/test/java/app/notesr/cli/parser/NotesParserTest.java
+++ src/test/java/app/notesr/cli/parser/NotesParserTest.java
@@ -1,9 +1,10 @@
 package app.notesr.cli.parser;
 
+import app.notesr.cli.TestBase;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-class NotesParserTest {
+class NotesParserTest extends TestBase {
 
     @BeforeEach
     void setUp() {
