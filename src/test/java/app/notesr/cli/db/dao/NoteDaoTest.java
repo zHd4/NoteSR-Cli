a768a5f0bdcd68dcf8430469b33eb120eada0aaf
zHd4 <computersystembase@gmail.com>
2025-02-18 15:26:04 +0100

    Refactor NoteDaoTest

--- src/test/java/app/notesr/cli/db/dao/NoteDaoTest.java
+++ src/test/java/app/notesr/cli/db/dao/NoteDaoTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class NoteDaoTest {
+public final class NoteDaoTest {
     private static final Faker FAKER = new Faker();
 
     private File dbFile;
