da0e743055b9c6bae894fcfe2b5c005723ac53ad
zHd4 <computersystembase@gmail.com>
2025-02-19 12:11:21 +0100

    Implement getAll in FileInfoDao

--- src/main/java/app/notesr/cli/db/dao/FileInfoDao.java
+++ src/main/java/app/notesr/cli/db/dao/FileInfoDao.java
@@ -5,9 +5,13 @@ import app.notesr.cli.model.FileInfo;
 import lombok.RequiredArgsConstructor;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import static app.notesr.cli.db.DbUtils.dateTimeToString;
+import static app.notesr.cli.db.DbUtils.parseDateTime;
 
 @RequiredArgsConstructor
 public final class FileInfoDao {
@@ -29,5 +33,31 @@ public final class FileInfoDao {
 
             stmt.executeUpdate();
         }
+    }
+
+    public Set<FileInfo> getAll() throws SQLException {
+        Set<FileInfo> results = new LinkedHashSet<>();
+
+        String sql = "SELECT * FROM files_info";
+        try (PreparedStatement stmt = dbConnection.getConnection().prepareStatement(sql)) {
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                FileInfo fileInfo = FileInfo.builder()
+                        .id(rs.getString(1))
+                        .noteId(rs.getString(2))
+                        .name(rs.getString(3))
+                        .type(rs.getString(4))
+                        .thumbnail(rs.getBytes(5))
+                        .size(rs.getLong(6))
+                        .createdAt(parseDateTime(rs.getString(7)))
+                        .updatedAt(parseDateTime(rs.getString(8)))
+                        .build();
+
+                results.add(fileInfo);
+            }
+        }
+
+        return results;
     }
 }
