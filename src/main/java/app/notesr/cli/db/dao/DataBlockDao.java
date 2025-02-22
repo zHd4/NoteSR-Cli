f250bb10f58fe755a33c859a8a68e6d6d3ef29b1
zHd4 <computersystembase@gmail.com>
2025-02-22 19:54:01 +0100

    Implement getById in DataBlockDao

--- src/main/java/app/notesr/cli/db/dao/DataBlockDao.java
+++ src/main/java/app/notesr/cli/db/dao/DataBlockDao.java
@@ -42,4 +42,24 @@ public final class DataBlockDao {
 
         return results;
     }
+
+    public DataBlock getById(String id) throws SQLException {
+        String sql = "SELECT * FROM data_blocks WHERE id = ?";
+
+        try (PreparedStatement stmt = db.getConnection().prepareStatement(sql)) {
+            stmt.setString(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return DataBlock.builder()
+                        .id(rs.getString(1))
+                        .fileId(rs.getString(2))
+                        .order(rs.getLong(3))
+                        .data(rs.getBytes(4))
+                        .build();
+            }
+        }
+
+        return null;
+    }
 }
