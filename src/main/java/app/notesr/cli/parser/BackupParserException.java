405aca429693b3fba20a148794cb0c206127c6a4
zHd4 <computersystembase@gmail.com>
2025-02-24 17:05:59 +0100

    Create BackupParserException

---
+++ src/main/java/app/notesr/cli/parser/BackupParserException.java
@@ -0,0 +1,7 @@
+package app.notesr.cli.parser;
+
+public class BackupParserException extends RuntimeException {
+    public BackupParserException(String message) {
+        super(message);
+    }
+}
