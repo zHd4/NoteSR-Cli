af5174a462d58c83bc324dd3271fcb4f0ab45e27
zHd4 <computersystembase@gmail.com>
2025-02-24 16:45:21 +0100

    Create ParserV1

---
+++ src/main/java/app/notesr/cli/parser/v1/ParserV1.java
@@ -0,0 +1,16 @@
+package app.notesr.cli.parser.v1;
+
+import app.notesr.cli.parser.Parser;
+
+import java.nio.file.Path;
+
+public class ParserV1 extends Parser {
+    public ParserV1(Path backupPath, Path outputDbPath) {
+        super(backupPath, outputDbPath);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
