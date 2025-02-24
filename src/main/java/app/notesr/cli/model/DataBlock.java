1eae63b46cf81fc8cb0ab8b0f2d8926e0bb22337
zHd4 <computersystembase@gmail.com>
2025-02-24 17:06:40 +0100

    Update DataBlock

--- src/main/java/app/notesr/cli/model/DataBlock.java
+++ src/main/java/app/notesr/cli/model/DataBlock.java
@@ -1,8 +1,12 @@
 package app.notesr.cli.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class DataBlock {
