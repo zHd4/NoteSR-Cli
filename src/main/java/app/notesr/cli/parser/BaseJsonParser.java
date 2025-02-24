f99d47a72d6c8e1cdefd4557ca3ca5d41cab7af7
zHd4 <computersystembase@gmail.com>
2025-02-24 17:06:54 +0100

    Create BaseJsonParser

---
+++ src/main/java/app/notesr/cli/parser/BaseJsonParser.java
@@ -0,0 +1,28 @@
+package app.notesr.cli.parser;
+
+import app.notesr.cli.db.DbConnection;
+import com.fasterxml.jackson.core.JsonParser;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+
+@RequiredArgsConstructor
+public class BaseJsonParser {
+    protected final JsonParser parser;
+    protected final DateTimeFormatter timestampFormatter;
+
+    protected boolean skipTo(String targetField) throws IOException {
+        String currentField = parser.getCurrentName();
+
+        while (currentField == null || !currentField.equals(targetField)) {
+            if (parser.nextToken() == null) {
+                return false;
+            }
+
+            currentField = parser.getCurrentName();
+        }
+
+        return true;
+    }
+}
