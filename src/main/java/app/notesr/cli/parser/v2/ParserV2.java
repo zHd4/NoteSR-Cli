553f3caea8ade29422ee6051cd72487581b86e0c
zHd4 <computersystembase@gmail.com>
2025-02-25 13:39:22 +0100

    Update ParserV2

--- src/main/java/app/notesr/cli/parser/v2/ParserV2.java
+++ src/main/java/app/notesr/cli/parser/v2/ParserV2.java
@@ -1,26 +1,51 @@
 package app.notesr.cli.parser.v2;
 
+import app.notesr.cli.db.DbConnection;
 import app.notesr.cli.parser.BackupIOException;
+import app.notesr.cli.parser.NotesJsonParser;
 import app.notesr.cli.parser.Parser;
 import app.notesr.cli.util.ZipUtils;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.time.format.DateTimeFormatter;
 
 public class ParserV2 extends Parser {
+    private static final String NOTES_JSON_FILE_NAME = "notes.json";
+    private static final String FILES_INFO_JSON_FILE_NAME = "files_info.json";
+    private static final String DATA_BLOCKS_DIR_NAME = "data_blocks";
+
+    private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     private final Path tempDirPath;
+    private final DbConnection db;
 
     public ParserV2(Path backupPath, Path tempDirPath, Path outputDbPath) {
         super(backupPath, outputDbPath);
+
         this.tempDirPath = tempDirPath;
+        this.db = new DbConnection(outputDbPath.toString());
     }
 
     @Override
     public void run() {
         try {
             ZipUtils.unzip(backupPath.toString(), tempDirPath.toString());
+
+            File notesJsonFile = new File(tempDirPath.toString(), NOTES_JSON_FILE_NAME);
+            NotesJsonParser notesJsonParser = new NotesJsonParser(db, getJsonParser(notesJsonFile), DATETIME_FORMATTER);
+
+            notesJsonParser.transferToDb();
         } catch (IOException e) {
             throw new BackupIOException(e);
         }
+    }
+
+    private JsonParser getJsonParser(File jsonFile) throws IOException {
+        JsonFactory factory = new JsonFactory();
+        return factory.createParser(jsonFile);
     }
 }
