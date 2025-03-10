294b3e0a1fff37b8197d11fa82429f1c23f84894
zHd4 <computersystembase@gmail.com>
2025-03-10 18:46:15 +0100

    Create FilesJsonParserV2

---
+++ src/main/java/app/notesr/cli/parser/v2/FilesJsonParserV2.java
@@ -0,0 +1,26 @@
+package app.notesr.cli.parser.v2;
+
+import app.notesr.cli.db.DbConnection;
+import app.notesr.cli.model.DataBlock;
+import app.notesr.cli.parser.FilesJsonParser;
+import com.fasterxml.jackson.core.JsonParser;
+
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+
+public class FilesJsonParserV2 extends FilesJsonParser {
+
+    public FilesJsonParserV2(DbConnection db, JsonParser parser, DateTimeFormatter timestampFormatter) {
+        super(db, parser, timestampFormatter);
+    }
+
+    @Override
+    protected void transferFilesData() throws IOException {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    protected void parseDataBlockObject(DataBlock dataBlock) throws IOException {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
