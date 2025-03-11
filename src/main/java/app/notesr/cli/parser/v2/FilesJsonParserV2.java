7aadbcb1922d5f43b8b6a396fa8365b5d74d369a
zHd4 <computersystembase@gmail.com>
2025-03-11 13:28:17 +0100

    Implement FilesJsonParserV2

--- src/main/java/app/notesr/cli/parser/v2/FilesJsonParserV2.java
+++ src/main/java/app/notesr/cli/parser/v2/FilesJsonParserV2.java
@@ -3,24 +3,90 @@ package app.notesr.cli.parser.v2;
 import app.notesr.cli.db.DbConnection;
 import app.notesr.cli.model.DataBlock;
 import app.notesr.cli.parser.FilesJsonParser;
+import app.notesr.cli.parser.UnexpectedFieldException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.SQLException;
 import java.time.format.DateTimeFormatter;
 
 public final class FilesJsonParserV2 extends FilesJsonParser {
+    private static final String ROOT_NAME = "files_data_blocks";
+    private static final String DATA_BLOCKS_DIR_NAME = "data_blocks";
 
-    public FilesJsonParserV2(DbConnection db, JsonParser parser, DateTimeFormatter timestampFormatter) {
+    private final Path tempDirPath;
+
+    public FilesJsonParserV2(DbConnection db, JsonParser parser, Path tempDirPath, DateTimeFormatter timestampFormatter) {
         super(db, parser, timestampFormatter);
+        this.tempDirPath = tempDirPath;
     }
 
     @Override
-    protected void transferFilesData() throws IOException {
-        throw new UnsupportedOperationException("Not implemented");
+    protected void transferFilesData() throws IOException, SQLException {
+        if (skipTo(ROOT_NAME)) {
+            if (parser.nextToken() == JsonToken.START_ARRAY) {
+                do {
+                    DataBlock dataBlock = new DataBlock();
+                    parseDataBlockObject(dataBlock);
+
+                    String id = dataBlock.getId();
+
+                    if (id != null) {
+                        dataBlock.setData(readDataBlock(id));
+                        dataBlockDao.add(dataBlock);
+                    }
+                } while (parser.nextToken() != JsonToken.END_ARRAY);
+            }
+        }
     }
 
     @Override
     protected void parseDataBlockObject(DataBlock dataBlock) throws IOException {
-        throw new UnsupportedOperationException("Not implemented");
+        String field;
+
+        while (parser.nextToken() != JsonToken.END_OBJECT) {
+            field = parser.getCurrentName();
+
+            if (field != null) {
+                switch (field) {
+                    case "id" -> {
+                        if (parser.getValueAsString().equals("id")) {
+                            continue;
+                        }
+
+                        dataBlock.setId(parser.getValueAsString());
+                    }
+
+                    case "file_id" -> {
+                        if (parser.getValueAsString().equals("file_id")) {
+                            continue;
+                        }
+
+                        dataBlock.setFileId(parser.getValueAsString());
+                    }
+
+                    case "order" -> {
+                        if (parser.getValueAsString().equals("order")) {
+                            continue;
+                        }
+
+                        dataBlock.setOrder(parser.getValueAsLong());
+                    }
+
+                    default -> {
+                        if (!field.equals(ROOT_NAME)) {
+                            throw new UnexpectedFieldException("Unexpected field: " + field);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private byte[] readDataBlock(String id) throws IOException {
+        return Files.readAllBytes(Path.of(tempDirPath.toString(), DATA_BLOCKS_DIR_NAME, id));
     }
 }
