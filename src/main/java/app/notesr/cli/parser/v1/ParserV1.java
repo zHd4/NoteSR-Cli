0cbc5efe3bae15ab89d772802ebc0bfcc6701648
zHd4 <computersystembase@gmail.com>
2025-03-13 16:32:27 +0100

    Implement ParserV1

--- src/main/java/app/notesr/cli/parser/v1/ParserV1.java
+++ src/main/java/app/notesr/cli/parser/v1/ParserV1.java
@@ -1,16 +1,41 @@
 package app.notesr.cli.parser.v1;
 
+import app.notesr.cli.db.DbConnection;
+import app.notesr.cli.parser.BackupDbException;
+import app.notesr.cli.parser.BackupIOException;
+import app.notesr.cli.parser.FilesJsonParser;
+import app.notesr.cli.parser.NotesJsonParser;
 import app.notesr.cli.parser.Parser;
+import com.fasterxml.jackson.core.JsonParser;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.sql.SQLException;
 
 public final class ParserV1 extends Parser {
+    private final DbConnection db;
+
     public ParserV1(Path backupPath, Path outputDbPath) {
         super(backupPath, outputDbPath);
+        this.db = new DbConnection(outputDbPath.toString());
     }
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not implemented");
+        try {
+            File backupFile = backupPath.toFile();
+            JsonParser jsonParser = getJsonParser(backupFile);
+
+            NotesJsonParser notesJsonParser = new NotesJsonParser(db, jsonParser, DATETIME_FORMATTER);
+            FilesJsonParser filesJsonParser = new FilesJsonParserV1(db, jsonParser, DATETIME_FORMATTER);
+
+            notesJsonParser.transferToDb();
+            filesJsonParser.transferToDb();
+        } catch (IOException e) {
+            throw new BackupIOException(e);
+        } catch (SQLException e) {
+            throw new BackupDbException(e);
+        }
     }
 }
