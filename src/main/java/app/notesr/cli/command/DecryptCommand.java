c71dd56b5a7d62d79fc50204a756fb0df2a33a17
zHd4 <computersystembase@gmail.com>
2025-03-17 15:50:08 +0100

    Add DecryptCommand

---
+++ src/main/java/app/notesr/cli/command/DecryptCommand.java
@@ -0,0 +1,24 @@
+package app.notesr.cli.command;
+
+import lombok.Getter;
+import picocli.CommandLine;
+
+@Getter
+@CommandLine.Command(name = "decrypt",
+        description = "Decrypts exported NoteSR .bak file and converts it to a SQLite database.")
+public class DecryptCommand implements Command {
+    @CommandLine.Parameters(index = "0", paramLabel = "file_path", description = "path to encrypted NoteSR .bak file")
+    private String encryptedBackupPath;
+
+    @CommandLine.Parameters(index = "1", paramLabel = "key_path", description = "path to exported key (text file)")
+    private String keyPath;
+
+    @CommandLine.Option(names = { "-o", "--output" }, description = "output SQLite database path")
+    private String outputFilePath;
+
+    @Override
+    public Integer call() {
+        System.out.println("Decrypt!");
+        return 0;
+    }
+}
