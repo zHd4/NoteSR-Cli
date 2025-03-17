a334cccbf64ea1dd553cee27b27c56c700aedef5
zHd4 <computersystembase@gmail.com>
2025-03-17 15:50:27 +0100

    Implement App

--- src/main/java/app/notesr/cli/App.java
+++ src/main/java/app/notesr/cli/App.java
@@ -1,7 +1,23 @@
 package app.notesr.cli;
 
-public class App {
+import app.notesr.cli.command.DecryptCommand;
+import picocli.CommandLine;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "notesr-cli",
+        description = "",
+        mixinStandardHelpOptions = true,
+        subcommands = {DecryptCommand.class})
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
-        // Will be implemented
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() {
+        System.out.println("Please use -h flag to see the available commands.");
+        System.out.println("No command provided. Exit.");
+        return 0;
     }
 }
