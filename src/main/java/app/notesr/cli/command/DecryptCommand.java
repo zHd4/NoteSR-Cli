edcc705f4d3059c55fa56595775d68a591cf3350
zHd4 <computersystembase@gmail.com>
2025-03-18 21:49:19 +0100

    Update DecryptCommand

--- src/main/java/app/notesr/cli/command/DecryptCommand.java
+++ src/main/java/app/notesr/cli/command/DecryptCommand.java
@@ -1,12 +1,23 @@
 package app.notesr.cli.command;
 
+import app.notesr.cli.crypto.Aes;
+import app.notesr.cli.crypto.BackupDecryptor;
+import app.notesr.cli.crypto.CryptoKey;
+import app.notesr.cli.crypto.CryptoKeyUtils;
+import app.notesr.cli.crypto.exception.BackupDecryptionException;
 import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 
 @Getter
 @CommandLine.Command(name = "decrypt",
@@ -33,11 +44,48 @@ public final class DecryptCommand implements Command {
             keyFile = getFile(this.keyPath);
         } catch (NoSuchFileException e) {
             LOGGER.error(e.getMessage());
-            return 1;
+            return 2;
+        }
+
+        CryptoKey cryptoKey;
+
+        try {
+            cryptoKey = getCryptoKey(keyFile);
+        } catch (IOException e) {
+            LOGGER.error("{}: an error occurred while reading", keyPath);
+            return 2;
+        }
+
+        File tempDecryptedBackup;
+
+        try {
+            tempDecryptedBackup = decryptBackup(encryptedBackupFile, cryptoKey);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e); // Already validated
+        } catch (BackupDecryptionException e) {
+            LOGGER.error("{}: failed to decrypt, file corrupted or invalid key", encryptedBackupPath);
+            return 7;
         }
 
         LOGGER.info("Decrypt!");
         return 0;
+    }
+
+    private File decryptBackup(File encryptedBackup, CryptoKey cryptoKey) throws FileNotFoundException,
+            BackupDecryptionException {
+        File decryptedBackup = Path.of(encryptedBackup.getAbsolutePath() + "_decrypted").toFile();
+
+        BackupDecryptor decryptor = new BackupDecryptor(cryptoKey);
+        FileInputStream inputStream = new FileInputStream(encryptedBackup);
+        FileOutputStream outputStream = new FileOutputStream(decryptedBackup);
+
+        decryptor.decrypt(inputStream, outputStream);
+        return decryptedBackup;
+    }
+
+    private CryptoKey getCryptoKey(File keyFile) throws IOException {
+        String hexKey = Files.readString(keyFile.toPath());
+        return CryptoKeyUtils.hexToCryptoKey(hexKey, Aes.KEY_GENERATOR_ALGORITHM);
     }
 
     private File getFile(String path) throws NoSuchFileException {
