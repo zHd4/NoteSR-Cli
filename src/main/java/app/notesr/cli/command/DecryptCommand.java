fd52b912420aaae6c910b24b223c18682f5168a8
zHd4 <computersystembase@gmail.com>
2025-03-19 15:38:10 +0100

    Update DecryptCommand

--- src/main/java/app/notesr/cli/command/DecryptCommand.java
+++ src/main/java/app/notesr/cli/command/DecryptCommand.java
@@ -5,6 +5,11 @@ import app.notesr.cli.crypto.BackupDecryptor;
 import app.notesr.cli.crypto.CryptoKey;
 import app.notesr.cli.crypto.CryptoKeyUtils;
 import app.notesr.cli.crypto.exception.BackupDecryptionException;
+import app.notesr.cli.parser.BackupDbException;
+import app.notesr.cli.parser.BackupIOException;
+import app.notesr.cli.parser.BackupParser;
+import app.notesr.cli.parser.BackupParserException;
+import app.notesr.cli.parser.UnexpectedFieldException;
 import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,12 +22,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.Objects;
 
 @Getter
 @CommandLine.Command(name = "decrypt",
         description = "Decrypts exported NoteSR .bak file and converts it to a SQLite database.")
 public final class DecryptCommand implements Command {
     private static final Logger LOGGER = LoggerFactory.getLogger(DecryptCommand.class);
+
+    private static final int SUCCESS = 0;
+    private static final int FILE_RW_ERROR = 2;
+    private static final int DB_WRITING_ERROR = 5;
+    private static final int DECRYPTION_ERROR = 7;
 
     @CommandLine.Parameters(index = "0", paramLabel = "file_path", description = "path to encrypted NoteSR .bak file")
     private String encryptedBackupPath;
@@ -37,25 +49,24 @@ public final class DecryptCommand implements Command {
     public Integer call() {
         File encryptedBackupFile;
         File keyFile;
+        CryptoKey cryptoKey;
+        File tempDecryptedBackup;
+        File outputFile;
 
         try {
             encryptedBackupFile = getFile(this.encryptedBackupPath);
             keyFile = getFile(this.keyPath);
         } catch (NoSuchFileException e) {
             LOGGER.error(e.getMessage());
-            return 2;
+            return FILE_RW_ERROR;
         }
-
-        CryptoKey cryptoKey;
 
         try {
             cryptoKey = getCryptoKey(keyFile);
         } catch (IOException e) {
             LOGGER.error("{}: an error occurred while reading", keyPath);
-            return 2;
+            return FILE_RW_ERROR;
         }
-
-        File tempDecryptedBackup;
 
         try {
             tempDecryptedBackup = decryptBackup(encryptedBackupFile, cryptoKey);
@@ -63,11 +74,21 @@ public final class DecryptCommand implements Command {
             throw new RuntimeException(e); // Already validated
         } catch (BackupDecryptionException e) {
             LOGGER.error("{}: failed to decrypt, file corrupted or invalid key", encryptedBackupPath);
-            return 7;
+            return DECRYPTION_ERROR;
         }
 
-        LOGGER.info("Decrypt!");
-        return 0;
+        outputFile = getOutputFilePath(encryptedBackupFile.getAbsolutePath(), outputFilePath).toFile();
+
+        try {
+            BackupParser backupParser = new BackupParser(tempDecryptedBackup.toPath(), outputFile.toPath());
+            backupParser.run();
+        } catch (BackupIOException | BackupParserException | UnexpectedFieldException e) {
+            return FILE_RW_ERROR;
+        } catch (BackupDbException e) {
+            return DB_WRITING_ERROR;
+        }
+
+        return SUCCESS;
     }
 
     private File decryptBackup(File encryptedBackup, CryptoKey cryptoKey) throws FileNotFoundException,
@@ -85,6 +106,10 @@ public final class DecryptCommand implements Command {
     private CryptoKey getCryptoKey(File keyFile) throws IOException {
         String hexKey = Files.readString(keyFile.toPath());
         return CryptoKeyUtils.hexToCryptoKey(hexKey, Aes.KEY_GENERATOR_ALGORITHM);
+    }
+
+    private Path getOutputFilePath(String inputPathStr, String outputPathStr) {
+        return Path.of(Objects.requireNonNullElseGet(outputPathStr, () -> inputPathStr + ".db"));
     }
 
     private File getFile(String path) throws NoSuchFileException {
