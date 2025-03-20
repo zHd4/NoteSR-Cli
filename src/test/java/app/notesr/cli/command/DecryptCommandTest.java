30b4e2244986f8699e72fe4db31a6e8557a5ebd7
zHd4 <computersystembase@gmail.com>
2025-03-20 11:05:59 +0100

    Implement testWithInvalidFilesPaths in DecryptCommandTest

--- src/test/java/app/notesr/cli/command/DecryptCommandTest.java
+++ src/test/java/app/notesr/cli/command/DecryptCommandTest.java
@@ -2,6 +2,8 @@ package app.notesr.cli.command;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import picocli.CommandLine;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,6 +20,16 @@ public class DecryptCommandTest {
     @Test
     public void testWithoutArgs() {
         int exitCode = cmd.execute();
+        assertEquals(DecryptCommand.FILE_RW_ERROR, exitCode, "Expected code " + DecryptCommand.FILE_RW_ERROR);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"C:\\folder\\..\\NUL\\file", "/////some///weird//path///file"})
+    public void testWithInvalidFilesPaths(String path) {
+        String filePath = path + ".notesr.bak";
+        String keyPath = path + ".txt";
+
+        int exitCode = cmd.execute(filePath, keyPath);
         assertEquals(DecryptCommand.FILE_RW_ERROR, exitCode, "Expected code " + DecryptCommand.FILE_RW_ERROR);
     }
 }
