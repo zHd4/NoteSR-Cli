4e162d7f18a903e0aa3c4cc147c056e45203a8d4
zHd4 <computersystembase@gmail.com>
2025-04-21 18:28:01 +0200

    Update log message in DecryptCommand to clarify decryption process

--- src/main/java/app/notesr/cli/command/DecryptCommand.java
+++ src/main/java/app/notesr/cli/command/DecryptCommand.java
@@ -84,7 +84,7 @@ public final class DecryptCommand implements Command {
         currentProcessAnimation = new CliSpinner("Decrypting");
 
         try {
-            log.info("Starting decryption of {}", encryptedBackupPath);
+            log.info("Decrypting {}", encryptedBackupPath);
 
             currentProcessAnimation.start();
             tempDecryptedBackup = decryptBackup(encryptedBackupFile, cryptoKey);
