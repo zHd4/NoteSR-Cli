366c6c82994faa33a63ebceea3122a2fca4628d6
zHd4 <computersystembase@gmail.com>
2025-05-03 21:36:15 +0200

    Implement methods for zipping in ZipUtils

--- src/main/java/app/notesr/cli/util/ZipUtils.java
+++ src/main/java/app/notesr/cli/util/ZipUtils.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class ZipUtils {
     public static boolean isZipArchive(String path) throws IOException {
@@ -25,6 +26,22 @@ public class ZipUtils {
         }
 
         return false;
+    }
+
+    public static void zipDirectory(String sourceDirPath, String output, Thread thread) throws
+            IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(output);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+
+        try (fileOutputStream; zipOutputStream) {
+            File sourceDir = new File(sourceDirPath);
+
+            if (!sourceDir.isDirectory()) {
+                throw new IllegalArgumentException("sourceDirPath must be a directory");
+            }
+
+            zipFilesRecursively(sourceDir, sourceDir, zipOutputStream, thread);
+        }
     }
 
     public static void unzip(String zipPath, String destDir) throws IOException {
@@ -66,6 +83,50 @@ public class ZipUtils {
 
                 zipInputStream.closeEntry();
             }
+        }
+    }
+
+    private static void zipFilesRecursively(
+            File rootDir,
+            File currentDir,
+            ZipOutputStream zipOutputStream,
+            Thread thread)
+            throws IOException {
+        if (thread != null && thread.isInterrupted()) {
+            return;
+        }
+
+        File[] files = currentDir.listFiles();
+
+        if (files == null) {
+            return;
+        }
+
+        for (File file : files) {
+            String relativePath = rootDir.toURI().relativize(file.toURI()).getPath();
+
+            if (file.isDirectory()) {
+                zipFilesRecursively(rootDir, file, zipOutputStream, thread);
+            } else {
+                zipFile(file, relativePath, zipOutputStream);
+            }
+        }
+    }
+
+    private static void zipFile(File file, String zipEntryName, ZipOutputStream zipOutputStream)
+            throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            ZipEntry zipEntry = new ZipEntry(zipEntryName);
+            zipOutputStream.putNextEntry(zipEntry);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                zipOutputStream.write(buffer, 0, bytesRead);
+            }
+
+            zipOutputStream.closeEntry();
         }
     }
 
