6a8acb36340d0c016e804fd33bef4ab8648d48e2
zHd4 <computersystembase@gmail.com>
2025-05-06 14:16:20 +0200

    Implement FileDataExporter

---
+++ src/main/java/app/notesr/cli/compiler/FileDataExporter.java
@@ -0,0 +1,38 @@
+package app.notesr.cli.compiler;
+
+import app.notesr.cli.db.dao.DataBlockDao;
+import app.notesr.cli.model.DataBlock;
+import lombok.RequiredArgsConstructor;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.SQLException;
+
+import static java.util.Objects.requireNonNull;
+
+@RequiredArgsConstructor
+class FileDataExporter implements Exporter {
+    private final File outputDir;
+    private final DataBlockDao dataBlockDao;
+
+    @Override
+    public void export() throws IOException, SQLException {
+        if (outputDir.exists()) {
+            if (!outputDir.isDirectory()) {
+                throw new IllegalArgumentException(outputDir.getAbsolutePath() + " isn't directory");
+            }
+        } else {
+            if (!outputDir.mkdir()) {
+                throw new IOException("Failed to create temporary directory " + outputDir.getAbsolutePath());
+            }
+        }
+
+        for (DataBlock dataBlock : dataBlockDao.getAllDataBlocksWithoutData()) {
+            File blockDataFile = new File(outputDir, dataBlock.getId());
+            byte[] blockData = requireNonNull(dataBlockDao.getById(dataBlock.getId())).getData();
+
+            Files.write(blockDataFile.toPath(), blockData);
+        }
+    }
+}
