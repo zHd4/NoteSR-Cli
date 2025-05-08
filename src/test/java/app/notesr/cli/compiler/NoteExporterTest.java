d4444ba83b89664864dc8fd827b5ec6269f97adc
zHd4 <computersystembase@gmail.com>
2025-05-08 13:37:50 +0200

    Convert NoteExporterTest to mock-based test

--- src/test/java/app/notesr/cli/compiler/NoteExporterTest.java
+++ src/test/java/app/notesr/cli/compiler/NoteExporterTest.java
@@ -1,13 +1,10 @@
 package app.notesr.cli.compiler;
 
-import app.notesr.cli.db.DbConnection;
 import app.notesr.cli.db.dao.NoteDao;
 import app.notesr.cli.model.Note;
-import app.notesr.cli.util.DbUtils;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,35 +24,36 @@ import java.nio.file.Path;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static app.notesr.cli.db.DbUtils.truncateDateTime;
 import static app.notesr.cli.util.PathUtils.getTempPath;
 import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-class NoteExporterIntegrationTest {
-    private static final String NOTES_TABLE_NAME = "notes";
+class NoteExporterTest {
     private static final String NOTES_ARRAY_NAME = "notes";
     private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private static final Faker FAKER = new Faker();
     private static final int TEST_NOTES_COUNT = 5;
 
-    private DbConnection db;
     private JsonGenerator jsonGenerator;
-    private NoteDao noteDao;
     private File outputFile;
+    private NoteDao noteDao;
+    private Set<Note> testNotes;
 
     @BeforeEach
     public void beforeEach() throws SQLException, IOException {
-        db = new DbConnection(":memory:");
-
-        noteDao = new NoteDao(db);
         outputFile = Path.of(getTempPath(randomUUID() + ".json").toString()).toFile();
         jsonGenerator = getTestJsonGenerator(outputFile);
+        noteDao = mock(NoteDao.class);
+        testNotes = generateTestNotes();
 
-        fillNotesTable(noteDao);
+        when(noteDao.getAll()).thenReturn(testNotes);
     }
 
     @Test
@@ -63,13 +61,10 @@ class NoteExporterIntegrationTest {
         NoteExporter noteExporter = new NoteExporter(jsonGenerator, noteDao, DATETIME_FORMATTER);
         noteExporter.export();
 
-        String notesTableJsonData = DbUtils.serializeTableAsJson(db.getConnection(), NOTES_TABLE_NAME);
         String outputFileJsonData = Files.readString(outputFile.toPath());
+        Set<Note> actual = deserializeResultFileData(outputFileJsonData);
 
-        List<Note> expected = deserializeTableData(notesTableJsonData);
-        List<Note> actual = deserializeResultFileData(outputFileJsonData);
-
-        assertEquals(expected, actual, "Notes are different");
+        assertEquals(testNotes, actual, "Notes are different");
     }
 
     @AfterEach
@@ -83,20 +78,7 @@ class NoteExporterIntegrationTest {
         }
     }
 
-    private List<Note> deserializeTableData(String json) throws JsonProcessingException {
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DATETIME_FORMATTER));
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        objectMapper.registerModule(javaTimeModule);
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        return objectMapper.readValue(json, new TypeReference<>() {});
-    }
-
-    private List<Note> deserializeResultFileData(String json) throws IOException {
+    private Set<Note> deserializeResultFileData(String json) throws IOException {
         JavaTimeModule javaTimeModule = new JavaTimeModule();
         javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DATETIME_FORMATTER));
 
@@ -109,7 +91,7 @@ class NoteExporterIntegrationTest {
         JsonNode root = objectMapper.readTree(json);
         JsonNode notesArray = root.get(NOTES_ARRAY_NAME);
 
-        return objectMapper.readerFor(new TypeReference<List<Note>>() {}).readValue(notesArray);
+        return objectMapper.readerFor(new TypeReference<Set<Note>>() {}).readValue(notesArray);
     }
 
     private JsonGenerator getTestJsonGenerator(File outputFile) throws IOException {
@@ -117,7 +99,9 @@ class NoteExporterIntegrationTest {
         return jsonFactory.createGenerator(outputFile, JsonEncoding.UTF8);
     }
 
-    private void fillNotesTable(NoteDao noteDao) throws SQLException {
+    private Set<Note> generateTestNotes() {
+        Set<Note> testNotes = new HashSet<>();
+
         for (int i = 0; i < TEST_NOTES_COUNT; i++) {
             Note testNote = Note.builder()
                     .id(randomUUID().toString())
@@ -126,7 +110,9 @@ class NoteExporterIntegrationTest {
                     .updatedAt(truncateDateTime(LocalDateTime.now()))
                     .build();
 
-            noteDao.add(testNote);
+            testNotes.add(testNote);
         }
+
+        return testNotes;
     }
 }
