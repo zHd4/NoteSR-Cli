9e00cbb01f4a0725e4c80c28070474bf24750212
zHd4 <computersystembase@gmail.com>
2025-05-13 18:08:54 +0200

    Update BackupIOException

--- src/main/java/app/notesr/cli/exception/BackupIOException.java
+++ src/main/java/app/notesr/cli/exception/BackupIOException.java
@@ -1,6 +1,10 @@
 package app.notesr.cli.exception;
 
 public class BackupIOException extends RuntimeException {
+    public BackupIOException(String message) {
+        super(message);
+    }
+
     public BackupIOException(Throwable cause) {
         super(cause);
     }
