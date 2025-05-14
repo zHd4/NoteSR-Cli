20bc30f4587eb2a0784ea5e755c0a490cb9e1dbf
zHd4 <computersystembase@gmail.com>
2025-05-14 18:21:25 +0200

    Remove unused PathUtils

--- src/test/java/app/notesr/cli/util/PathUtils.java
+++
@@ -1,9 +0,0 @@
-package app.notesr.cli.util;
-
-import java.nio.file.Path;
-
-public class PathUtils {
-    public static Path getTempPath(String pathPart) {
-        return Path.of(System.getProperty("java.io.tmpdir"), pathPart);
-    }
-}
