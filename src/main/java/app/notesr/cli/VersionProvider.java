22c39007b0ca64cffd275f70479d8490a88518d4
zHd4 <computersystembase@gmail.com>
2025-05-16 21:35:38 +0200

    Update VersionProvider to using version properties

--- src/main/java/app/notesr/cli/VersionProvider.java
+++ src/main/java/app/notesr/cli/VersionProvider.java
@@ -2,10 +2,38 @@ package app.notesr.cli;
 
 import picocli.CommandLine;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public final class VersionProvider implements CommandLine.IVersionProvider {
     @Override
     public String[] getVersion() {
-        String version = App.class.getPackage().getImplementationVersion();
-        return new String[] {"Version: " + (version != null ? version : "unknown")};
+        return new String[]{
+            "Version: " + getUtilityVersion(),
+            "Default NoteSR version: " + getDefaultNoteSrVersion()
+        };
+    }
+
+    public String getUtilityVersion() {
+        String version = getProperties().getProperty("utility.version");
+        return version != null ? version : "unknown";
+    }
+
+    public String getDefaultNoteSrVersion() {
+        String version = getProperties().getProperty("noteSr.defaultVersion");
+        return version != null ? version : "unknown";
+    }
+
+    private Properties getProperties() {
+        Properties props = new Properties();
+
+        try (InputStream in = getClass().getResourceAsStream("/version.properties")) {
+            props.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return props;
     }
 }
