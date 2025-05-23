66943d8618fb17e80d647b46001406f46a941aa5
zHd4 <computersystembase@gmail.com>
2025-05-23 17:36:36 +0200

    Refactor NoteFileInfoDao after linter check

--- src/main/java/app/notesr/cli/db/dao/NoteFileInfoDao.java
+++ src/main/java/app/notesr/cli/db/dao/NoteFileInfoDao.java
@@ -13,21 +13,20 @@ import java.util.Set;
 import static app.notesr.cli.db.DbUtils.parseDateTime;
 
 @RequiredArgsConstructor
-public class NoteFileInfoDao {
+public final class NoteFileInfoDao {
     private static final String GET_NOTE_FILE_INFO_OUTPUT_TABLE_QUERY = """
-                SELECT
-                	n.id,
-                	SUBSTR(n.name, 1, 30),
-                	SUBSTR(n.text, 1, 30),
-                	n.updated_at,
-                	(
-                		SELECT COUNT(*)
-                		FROM files_info f
-                		WHERE f.note_id = n.id
-                	) AS attached_files_count
-                FROM notes n
-                ORDER BY id;
-                """;
+            SELECT
+            \tn.id,
+            \tSUBSTR(n.name, 1, 30),
+            \tSUBSTR(n.text, 1, 30),
+            \tn.updated_at,
+            \t(
+            \t\tSELECT COUNT(*)
+            \t\tFROM files_info f
+            \t\tWHERE f.note_id = n.id
+            \t) AS attached_files_count
+            FROM notes n
+            ORDER BY id;""";
 
     private final DbConnection db;
 
