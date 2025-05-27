a76c49116d7b416dfe49fd11dbfd85edb5ba4d83
zHd4 <computersystembase@gmail.com>
2025-05-27 16:02:58 +0200

    Refactor UuidShortener to improve UUID validation and error handling

--- src/main/java/app/notesr/cli/util/UuidShortener.java
+++ src/main/java/app/notesr/cli/util/UuidShortener.java
@@ -2,6 +2,7 @@ package app.notesr.cli.util;
 
 import lombok.RequiredArgsConstructor;
 
+import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.util.Base64;
 import java.util.UUID;
@@ -9,16 +10,21 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public final class UuidShortener {
     private static final int UUID_BYTES_LENGTH = 16;
-    private static final int SHORT_UUID_LENGTH = 22;
-    private static final int LONG_UUID_LENGTH = 36;
+    private static final String UUID_REGEX = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-"
+            + "[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
+
+    private static final String SHORT_UUID_REGEX = "^[A-Za-z0-9_-]{22}$";
 
     private final String uuid;
 
     public String getShortUuid() {
-        if (uuid.length() == SHORT_UUID_LENGTH && uuid.matches("^[A-Za-z0-9_-]{22}$")) {
+        if (uuid.matches(SHORT_UUID_REGEX)) {
             return uuid;
         }
 
+        if (!uuid.matches(UUID_REGEX)) {
+            throw new IllegalArgumentException("Invalid UUID format: " + uuid);
+        }
         UUID uuidObj = UUID.fromString(uuid);
         ByteBuffer byteBuffer = ByteBuffer.wrap(new byte[UUID_BYTES_LENGTH]);
 
@@ -29,17 +35,29 @@ public final class UuidShortener {
     }
 
     public String getLongUuid() {
-        if (uuid.length() == LONG_UUID_LENGTH && uuid.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-"
-                + "[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")) {
+        if (uuid.matches(UUID_REGEX)) {
             return uuid;
         }
 
-        byte[] bytes = Base64.getUrlDecoder().decode(uuid);
-        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        if (!uuid.matches(SHORT_UUID_REGEX)) {
+            throw new IllegalArgumentException("Invalid shortened UUID: " + uuid);
+        }
 
-        long mostSigBits = byteBuffer.getLong();
-        long leastSigBits = byteBuffer.getLong();
+        try {
+            byte[] bytes = Base64.getUrlDecoder().decode(uuid);
 
-        return new UUID(mostSigBits, leastSigBits).toString();
+            if (bytes.length != UUID_BYTES_LENGTH) {
+                throw new IllegalArgumentException("Base64 UUID must be " + UUID_BYTES_LENGTH + " bytes long");
+            }
+
+            ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+
+            long mostSigBits = byteBuffer.getLong();
+            long leastSigBits = byteBuffer.getLong();
+
+            return new UUID(mostSigBits, leastSigBits).toString();
+        } catch (BufferUnderflowException | IllegalArgumentException e) {
+            throw new IllegalArgumentException("Error decoding UUID: " + uuid, e);
+        }
     }
 }
