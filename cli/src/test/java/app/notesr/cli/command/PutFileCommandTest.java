d297dd1c3e0bd1175edb21e9e3138d3f210f9ad9
zHd4 <computersystembase@gmail.com>
2025-06-13 11:12:53 +0200

    Implement PutFileCommandTest

---
+++ cli/src/test/java/app/notesr/cli/command/PutFileCommandTest.java
@@ -0,0 +1,141 @@
+package app.notesr.cli.command;
+
+import app.notesr.cli.db.DbConnection;
+import app.notesr.cli.db.dao.DataBlockEntityDao;
+import app.notesr.cli.db.dao.FileInfoEntityDao;
+import app.notesr.cli.db.dao.NoteEntityDao;
+import app.notesr.cli.model.DataBlock;
+import app.notesr.cli.model.FileInfo;
+import app.notesr.cli.model.Note;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import picocli.CommandLine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import static app.notesr.cli.command.Command.DB_ERROR;
+import static app.notesr.cli.command.Command.FILE_RW_ERROR;
+import static app.notesr.cli.command.Command.SUCCESS;
+import static app.notesr.cli.util.FixtureUtils.getFixturePath;
+import static app.notesr.cli.util.HashUtils.computeSha512;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class PutFileCommandTest {
+    private static final String BLANK_UUID = "123e4567-e89b-12d3-a456-426614174000";
+    private static final Random RANDOM = new Random();
+    private static final int MAX_TEST_FILE_BYTES = 1024 * 100;
+
+    private CommandLine cmd;
+
+    @TempDir
+    private Path tempDir;
+
+    @BeforeEach
+    void setUp() {
+        PutFileCommand putFileCommand = new PutFileCommand();
+        cmd = new CommandLine(putFileCommand);
+    }
+
+    @Test
+    void testCommand() throws SQLException, IOException, NoSuchAlgorithmException {
+        Path dbPath = getFixturePath("backup.db");
+        Path testFilePath = tempDir.resolve("test_file");
+        Note testNote = getRandomNote(dbPath);
+
+        byte[] testFileBytes = new byte[RANDOM.nextInt(1, MAX_TEST_FILE_BYTES)];
+
+        RANDOM.nextBytes(testFileBytes);
+        Files.write(testFilePath, testFileBytes);
+
+        int exitCode = cmd.execute(dbPath.toString(), testNote.getId(), testFilePath.toString());
+
+        assertEquals(SUCCESS, exitCode, "Expected code " + SUCCESS);
+        assertFileAdded(dbPath, testFilePath, testNote.getId());
+    }
+
+    @Test
+    void testCommandWithInvalidDbPath() {
+        String weirdPath = Path.of("/////some///weird//path///file").toString();
+
+        int exitCode = cmd.execute(weirdPath, BLANK_UUID, weirdPath);
+        assertEquals(FILE_RW_ERROR, exitCode, "Expected code " + FILE_RW_ERROR);
+    }
+
+    @Test
+    void testCommandWithInvalidDb() throws IOException {
+        Path dbPath = tempDir.resolve("invalid_db.db");
+        Path filePath = Path.of("/////some///weird//path///file");
+
+        Files.writeString(dbPath, "Lorem ipsum");
+
+        int exitCode = cmd.execute(dbPath.toString(), BLANK_UUID, filePath.toString());
+        assertEquals(DB_ERROR, exitCode, "Expected code " + DB_ERROR);
+    }
+
+    private void assertFileAdded(Path dbPath, Path filePath, String noteId) throws SQLException, IOException,
+            NoSuchAlgorithmException {
+        DbConnection db = new DbConnection(dbPath.toString());
+        FileInfoEntityDao fileInfoEntityDao = new FileInfoEntityDao(db);
+        DataBlockEntityDao dataBlockEntityDao = new DataBlockEntityDao(db);
+
+        String fileName = filePath.toFile().getName();
+        long fileSize = Files.size(filePath);
+        FileInfo fileInfo = getFileInfo(fileInfoEntityDao, noteId, fileName);
+
+        assertNotNull(fileInfo, "File info with name '" + fileName + "' and note id " + noteId + " not found");
+        assertEquals(fileSize, fileInfo.getSize(), "Unexpected file size");
+
+        Set<DataBlock> fileDataBlocks = getDataBlocks(dataBlockEntityDao, fileInfo.getId());
+        assertFalse(fileDataBlocks.isEmpty(), "No file data blocks found by file id "
+                + fileInfo.getId() + " (" + fileName + ")");
+
+        assertFileHash(filePath, fileDataBlocks);
+
+    }
+
+    private void assertFileHash(Path filePath, Set<DataBlock> fileDataBlocks)
+            throws NoSuchAlgorithmException, IOException {
+        String expectedFileHash = computeSha512(filePath.toString());
+        String actualFileHash = computeSha512(fileDataBlocks.stream().map(DataBlock::getData).toList());
+
+        assertEquals(expectedFileHash, actualFileHash, "Unexpected file hash");
+    }
+
+    private FileInfo getFileInfo(FileInfoEntityDao fileInfoEntityDao, String noteId, String fileName)
+            throws SQLException {
+        return fileInfoEntityDao.getByNoteId(noteId).stream()
+                .filter(file -> fileName.equals(file.getName()))
+                .findAny()
+                .orElse(null);
+    }
+
+    private Set<DataBlock> getDataBlocks(DataBlockEntityDao dataBlockEntityDao, String fileId) throws SQLException {
+        Set<DataBlock> dataBlocks = new LinkedHashSet<>();
+
+        for (String dataBlockId : dataBlockEntityDao.getIdsByFileId(fileId)) {
+            dataBlocks.add(dataBlockEntityDao.getById(dataBlockId));
+        }
+
+        return dataBlocks;
+    }
+
+    private Note getRandomNote(Path dbPath) throws SQLException {
+        DbConnection db = new DbConnection(dbPath.toString());
+        NoteEntityDao noteEntityDao = new NoteEntityDao(db);
+        List<Note> notes = new ArrayList<>(noteEntityDao.getAll());
+
+        return notes.get(RANDOM.nextInt(notes.size()));
+    }
+}
