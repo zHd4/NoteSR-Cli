d0f07d0c07c4cc35c41869387516aa8f5b2d6fe8
zHd4 <computersystembase@gmail.com>
2025-09-30 14:22:22 +0200

    Add AesGcmCryptor

---
+++ core/src/main/java/app/notesr/cli/crypto/AesGcmCryptor.java
@@ -0,0 +1,115 @@
+package app.notesr.cli.crypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AesGcmCryptor implements AesCryptor {
+    private static final int CHUNK_SIZE = 100_000;
+    private static final int IV_SIZE = 12;
+    private static final int TAG_LENGTH_BIT = 128;
+    private static final int TAG_LENGTH = TAG_LENGTH_BIT / 8;
+    private static final String CIPHER_ALGORITHM = "AES/GCM/NoPadding";
+
+    private final SecretKey key;
+
+    @Override
+    public void encrypt(InputStream in, OutputStream out)
+            throws GeneralSecurityException, IOException {
+
+        byte[] inBuffer = new byte[CHUNK_SIZE];
+        int bytesRead;
+
+        while ((bytesRead = in.read(inBuffer)) != -1) {
+            if (bytesRead == 0) {
+                continue;
+            }
+
+            byte[] iv = new byte[IV_SIZE];
+
+            SecureRandom.getInstanceStrong().nextBytes(iv);
+            out.write(iv);
+
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
+
+            byte[] encryptedBlock = cipher.doFinal(inBuffer, 0, bytesRead);
+            out.write(encryptedBlock);
+        }
+
+        out.flush();
+    }
+
+    @Override
+    public void decrypt(InputStream in, OutputStream out)
+            throws GeneralSecurityException, IOException {
+
+        byte[] ivBuffer = new byte[IV_SIZE];
+        byte[] inBuffer = new byte[CHUNK_SIZE + TAG_LENGTH];
+
+        while (true) {
+            int ivRead = readFully(in, ivBuffer);
+
+            if (ivRead == -1) {
+                break;
+            }
+
+            if (ivRead < IV_SIZE) {
+                throw new IOException("Ciphertext too short: missing IV");
+            }
+
+            int bytesRead = readBlock(in, inBuffer);
+            if (bytesRead <= 0) {
+                throw new IOException("Unexpected end of stream");
+            }
+
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(TAG_LENGTH_BIT, ivBuffer));
+
+            byte[] decryptedBlock = cipher.doFinal(inBuffer, 0, bytesRead);
+            out.write(decryptedBlock);
+        }
+
+        out.flush();
+    }
+
+    private static int readBlock(InputStream in, byte[] buffer) throws IOException {
+        int offset = 0;
+        int read;
+
+        while ((read = in.read(buffer, offset, buffer.length - offset)) > 0) {
+            offset += read;
+            if (offset == buffer.length) {
+                break;
+            }
+        }
+        return offset;
+    }
+
+    private static int readFully(InputStream in, byte[] buffer)
+            throws IOException {
+        final int offset = 0;
+        int totalRead = 0;
+
+        while (totalRead < IV_SIZE) {
+            int read = in.read(buffer, offset + totalRead, IV_SIZE - totalRead);
+
+            if (read == -1) {
+                break;
+            }
+
+            totalRead += read;
+        }
+
+        return totalRead == 0 ? -1 : totalRead;
+    }
+}
