53fb5aee64173066b06c7283e3382075535be143
zHd4 <computersystembase@gmail.com>
2025-09-30 19:31:53 +0200

    Refactor BackupIOException to accept IOException as a cause

--- core/src/main/java/app/notesr/cli/exception/BackupIOException.java
+++ core/src/main/java/app/notesr/cli/exception/BackupIOException.java
@@ -1,11 +1,13 @@
 package app.notesr.cli.exception;
 
+import java.io.IOException;
+
 public class BackupIOException extends RuntimeException {
     public BackupIOException(String message) {
         super(message);
     }
 
-    public BackupIOException(Throwable cause) {
+    public BackupIOException(IOException cause) {
         super(cause);
     }
 }
