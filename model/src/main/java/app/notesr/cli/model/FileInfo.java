382dd6ea64bc76974918b7c8f68e6cddf9fc50dc
zHd4 <computersystembase@gmail.com>
2025-10-03 19:41:34 +0200

    Add JSON annotations to FileInfo class for improved serialization

--- model/src/main/java/app/notesr/cli/model/FileInfo.java
+++ model/src/main/java/app/notesr/cli/model/FileInfo.java
@@ -1,5 +1,7 @@
 package app.notesr.cli.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +14,29 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class FileInfo {
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("note_id")
     private String noteId;
+
+    @JsonProperty("size")
     private Long size;
+
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("type")
     private String type;
+
+    @JsonProperty("thumbnail")
     private byte[] thumbnail;
+
+    @JsonProperty("created_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 }
