2b57c7c6bfbd02b1e981de73d3f8f395abda3b75
zHd4 <computersystembase@gmail.com>
2025-10-03 19:41:46 +0200

    Add JSON annotations to DataBlock class for improved serialization

--- model/src/main/java/app/notesr/cli/model/DataBlock.java
+++ model/src/main/java/app/notesr/cli/model/DataBlock.java
@@ -1,6 +1,7 @@
 package app.notesr.cli.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,11 +12,16 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class DataBlock {
+
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("file_id")
     private String fileId;
 
     @JsonAlias({"order"})
     private Long blockOrder;
 
+    @JsonProperty("data")
     private byte[] data;
 }
