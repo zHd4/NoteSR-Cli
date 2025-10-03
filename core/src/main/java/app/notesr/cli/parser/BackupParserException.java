7b0a7d4dfdf8751bc4bbdb77089c32ad95fa0474
zHd4 <computersystembase@gmail.com>
2025-10-03 14:49:27 +0200

    Add additional constructor to BackupParserException

--- core/src/main/java/app/notesr/cli/parser/BackupParserException.java
+++ core/src/main/java/app/notesr/cli/parser/BackupParserException.java
@@ -4,4 +4,8 @@ public class BackupParserException extends RuntimeException {
     public BackupParserException(String message) {
         super(message);
     }
+
+    public BackupParserException(String message, Throwable e) {
+        super(message, e);
+    }
 }
