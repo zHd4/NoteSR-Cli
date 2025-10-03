a1fc03d39604f181c492244de37675fb71cd6b87
zHd4 <computersystembase@gmail.com>
2025-10-03 19:41:25 +0200

    Add JSON annotations to Note class for improved serialization

--- model/src/main/java/app/notesr/cli/model/Note.java
+++ model/src/main/java/app/notesr/cli/model/Note.java
@@ -1,5 +1,7 @@
 package app.notesr.cli.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +14,20 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class Note {
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("text")
     private String text;
+
+    @JsonProperty("created_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 }
