4bd4504302f061fc37428d198bd0b4bda423bdad
zHd4 <computersystembase@gmail.com>
2025-10-05 15:59:57 +0200

    Refactor LocalDateTimeArgumentFactory to extend AbstractArgumentFactory and simplify argument building

--- db/src/main/java/app/notesr/cli/db/LocalDateTimeArgumentFactory.java
+++ db/src/main/java/app/notesr/cli/db/LocalDateTimeArgumentFactory.java
@@ -1,24 +1,23 @@
 package app.notesr.cli.db;
 
+import org.jdbi.v3.core.argument.AbstractArgumentFactory;
 import org.jdbi.v3.core.argument.Argument;
-import org.jdbi.v3.core.argument.ArgumentFactory;
 import org.jdbi.v3.core.config.ConfigRegistry;
 
-import java.lang.reflect.Type;
+import java.sql.Types;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
 
-class LocalDateTimeArgumentFactory implements ArgumentFactory {
-    private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+class LocalDateTimeArgumentFactory extends AbstractArgumentFactory<LocalDateTime> {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public LocalDateTimeArgumentFactory() {
+        super(Types.VARCHAR);
+    }
 
     @Override
-    public Optional<Argument> build(Type type, Object value, ConfigRegistry config) {
-        if (value instanceof LocalDateTime localDateTime) {
-            return Optional.of((position, statement, ctx) ->
-                    statement.setString(position, localDateTime.format(DATETIME_FORMATTER)));
-        }
-
-        return Optional.empty();
+    protected Argument build(LocalDateTime value, ConfigRegistry config) {
+        return (position, statement, ctx) ->
+                statement.setString(position, value.format(FORMATTER));
     }
 }
