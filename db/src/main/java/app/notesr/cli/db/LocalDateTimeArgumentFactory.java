4f28428bae9b3c4a37215adcbfabce3415e1f377
zHd4 <computersystembase@gmail.com>
2025-10-05 19:51:44 +0200

    Refactor LocalDateTimeArgumentFactory constructor to package-private

--- db/src/main/java/app/notesr/cli/db/LocalDateTimeArgumentFactory.java
+++ db/src/main/java/app/notesr/cli/db/LocalDateTimeArgumentFactory.java
@@ -11,7 +11,7 @@ import java.time.format.DateTimeFormatter;
 class LocalDateTimeArgumentFactory extends AbstractArgumentFactory<LocalDateTime> {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public LocalDateTimeArgumentFactory() {
+    LocalDateTimeArgumentFactory() {
         super(Types.VARCHAR);
     }
 
