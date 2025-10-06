4260ba092f7f45036aa276ed06904262a232c1cf
zHd4 <computersystembase@gmail.com>
2025-10-06 14:07:54 +0200

    Add VersionComparator

---
+++ util/src/main/java/app/notesr/cli/util/VersionComparator.java
@@ -0,0 +1,24 @@
+package app.notesr.cli.util;
+
+import java.util.Comparator;
+
+public final class VersionComparator implements Comparator<String> {
+    @Override
+    public int compare(String v1, String v2) {
+        String[] parts1 = v1.split("\\.");
+        String[] parts2 = v2.split("\\.");
+
+        int length = Math.max(parts1.length, parts2.length);
+
+        for (int i = 0; i < length; i++) {
+            int num1 = i < parts1.length ? Integer.parseInt(parts1[i]) : 0;
+            int num2 = i < parts2.length ? Integer.parseInt(parts2[i]) : 0;
+
+            if (num1 != num2) {
+                return Integer.compare(num1, num2);
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ util/src/test/java/app/notesr/cli/util/VersionComparatorTest.java
@@ -0,0 +1,37 @@
+package app.notesr.cli.util;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class VersionComparatorTest {
+    private final VersionComparator comparator = new VersionComparator();
+
+    @ParameterizedTest
+    @CsvSource({
+            //equal versions
+            "1.0, 1.0, 0",
+            "2.5.3, 2.5.3, 0",
+            "1.0, 1.0.0, 0",
+            "4.0.0.0, 4, 0",
+
+            // first bigger
+            "1.1, 1.0, 1",
+            "2.0.1, 2.0.0, 1",
+            "10.0, 2.9.9, 1",
+            "1.0.1, 1.0, 1",
+            "1.2.10, 1.2.2, 1",
+
+            // second bigger
+            "1.0, 1.1, -1",
+            "2.0.0, 2.0.1, -1",
+            "2.9.9, 10.0, -1",
+            "1.0, 1.0.1, -1",
+            "3.4.5.6, 3.4.5.7, -1"
+    })
+    void testCompareVersions(String v1, String v2, int expectedSign) {
+        int result = comparator.compare(v1, v2);
+        assertEquals(expectedSign, Integer.signum(result));
+    }
+}
