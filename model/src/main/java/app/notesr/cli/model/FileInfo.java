29b0007e697c0f54e3a52ac482b847ac8c9414a2
zHd4 <computersystembase@gmail.com>
2025-10-07 19:06:50 +0200

    Add JsonAlias to FileInfo.noteId

--- model/src/main/java/app/notesr/cli/model/FileInfo.java
+++ model/src/main/java/app/notesr/cli/model/FileInfo.java
@@ -1,5 +1,6 @@
 package app.notesr.cli.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -18,6 +19,7 @@ public class FileInfo {
     private String id;
 
     @JsonProperty("note_id")
+    @JsonAlias({"noteId"})
     private String noteId;
 
     @JsonProperty("size")
