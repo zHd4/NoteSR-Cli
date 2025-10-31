233f810e75059ce715683dd9095879f7c9b6d0c7
zHd4 <computersystembase@gmail.com>
2025-10-31 23:31:01 +0100

    Fix PutFileCommandTest

--- cli/src/test/java/app/notesr/cli/command/PutFileCommandTest.java
+++ cli/src/test/java/app/notesr/cli/command/PutFileCommandTest.java
@@ -49,7 +49,7 @@ class PutFileCommandTest {
 
     @Test
     void testCommand() throws IOException, NoSuchAlgorithmException {
-        Path dbPath = getFixturePath("backup.db", tempDir);
+        Path dbPath = getFixturePath("shared/backup.db", tempDir);
         Path testFilePath = tempDir.resolve("test_file");
         Note testNote = getRandomNote(dbPath);
 
